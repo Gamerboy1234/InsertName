47504b059e774d21e5ed0bb551c97d96ac9ec2c4
Gamerboy1234 <wells.justin70@yahoo.com>
2018-06-04 14:38:11 -0600

    spell upgrades sort of working now
    well the upgrade spell goes on to the hot bar but it doesn't remove the old spell

---
+++ Source/InsertNameV2/Mouse_Aiming.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Mouse_Aiming.h"
+
+Mouse_Aiming::Mouse_Aiming()
+{
+}
+
+Mouse_Aiming::~Mouse_Aiming()
+{
+}
---
+++ Source/InsertNameV2/Mouse_Aiming.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class INSERTNAMEV2_API Mouse_Aiming
+{
+public:
+	Mouse_Aiming();
+	~Mouse_Aiming();
+};
