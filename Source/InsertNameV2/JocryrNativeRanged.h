40ef0ef69fb8a60c660752133cbbd984ac591d0f
bossmodegoat <egonthedestroyer321@gmail.com>
2019-01-05 17:39:38 -0700

    basic joquire ai added

---
+++ Source/InsertNameV2/JocryrNativeRanged.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "JocryrNativeRanged.generated.h"
+
+UCLASS()
+class INSERTNAMEV2_API AJocryrNativeRanged : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AJocryrNativeRanged();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	
+	
+};
