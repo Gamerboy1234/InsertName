d031c05905a89af40b0742a6c63ac6640f06e627
Reetro <thegamerboy7@gmail.com>
2020-04-22 09:10:41 -0600

    Setup basic controller detection

--- Source/InsertNameV2/Private/WardenController.cpp
+++ Source/InsertNameV2/Private/WardenController.cpp
@@ -6,6 +6,13 @@
 #include "Master_Gun.h"
 #include "InsertNameV2.h"
 
+AWardenController::AWardenController()
+{
+  bIsUsingGamepad = false;
+  bResetGamepadDetectionAfterNoInput = true;
+  GamepadTimeout = 5.f;
+}
+
 void AWardenController::BuildInputStack(TArray< UInputComponent* >& InputStack)
 {
   APaperWarden* LocalPlayer = Cast<APaperWarden>(GetPawnOrSpectator());
@@ -24,4 +31,32 @@ void AWardenController::BuildInputStack(TArray< UInputComponent* >& InputStack)
   }
 
   Super::BuildInputStack(InputStack);
+}
+
+bool AWardenController::InputAxis(FKey Key, float Delta, float DeltaTime, int32 NumSamples, bool bGamepad)
+{
+  bool ret = Super::InputAxis(Key, Delta, DeltaTime, NumSamples, bGamepad);
+  _UpdateGamepad(bGamepad);
+  return ret;
+}
+
+bool AWardenController::InputKey(FKey Key, EInputEvent EventType, float AmountDepressed, bool bGamepad)
+{
+  bool ret = Super::InputKey(Key, EventType, AmountDepressed, bGamepad);
+  _UpdateGamepad(bGamepad);
+  return ret;
+}
+
+void AWardenController::PlayerTick(float DeltaTime)
+{
+  Super::PlayerTick(DeltaTime);
+  if (IsLocalController() && bResetGamepadDetectionAfterNoInput && bIsUsingGamepad)
+  {
+    float now = GetWorld()->TimeSeconds;
+    if (now > LastGamepadInputTime + GamepadTimeout)
+    {
+      bIsUsingGamepad = false;
+      ControllerDisconnected();
+    }
+  }
 }
--- Source/InsertNameV2/Public/WardenController.h
+++ Source/InsertNameV2/Public/WardenController.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "Engine/World.h"
 #include "WardenController.generated.h"
 
 /**
@@ -16,6 +17,49 @@ class INSERTNAMEV2_API AWardenController : public APlayerController
 
 public:
 
+  AWardenController();
+
+  /* Used to prioritize weapon input  */
   virtual void BuildInputStack(TArray< UInputComponent* >& InputStack) override;
-	
+
+  /** Can be called to check and see if the player is currently using a gamepad */
+  UPROPERTY(Transient, BlueprintReadOnly, Category = "Gamepad Settings")
+  bool bIsUsingGamepad;
+
+  /** Specifies whether we set `bIsUsingGamepad` to `false` if we receive no input for a period of time. If set to 'true', GamepadTimeout will control how long
+      we need to go without receiving input before we set `bIsUsingGamepad` to `false`. */
+  UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Gamepad Settings")
+  bool bResetGamepadDetectionAfterNoInput;
+
+  /** How long we can go without receiving a gamepad input before we assume they've stopped using the gamepad */
+  UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Gamepad Settings", meta = (EditCondition = "bResetGamepadDetectionAfterNoInput"))
+  float GamepadTimeout;
+
+  // Overrides
+  virtual bool InputAxis(FKey Key, float Delta, float DeltaTime, int32 NumSamples, bool bGamepad) override;
+  virtual bool InputKey(FKey Key, EInputEvent EventType, float AmountDepressed, bool bGamepad) override;
+  virtual void PlayerTick(float DeltaTime) override;
+
+  UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = "Controller Events")
+  void ControllerConnected();
+
+  UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = "Controller Events")
+  void ControllerDisconnected();
+
+protected:
+
+  /** Common logic needed in both `InputAxis()` and `InputKey()` */
+  FORCEINLINE void _UpdateGamepad(bool bGamepad)
+  {
+    bIsUsingGamepad = bGamepad;
+    if (bGamepad)
+    {
+      LastGamepadInputTime = GetWorld()->TimeSeconds;
+      ControllerConnected();
+    }
+  }
+
+  // Used to keep track of when we last saw gamepad input
+  UPROPERTY(Transient, BlueprintReadOnly, Category = "Gamepad Settings")
+  float LastGamepadInputTime;
 };
