e3475dbba75f5a899ffc8705f7f8e05dbcf9a331
Reetro <thegamerboy7@gmail.com>
2020-05-01 09:26:33 -0600

    Revert "Moved moveable platform code to blueprint"
    This reverts commit 1c52ccb02f5a9a890898beddc966465d3cafd9a0.

--- Source/InsertNameV2/Public/Master_MoveablePlatform.h
+++ Source/InsertNameV2/Public/Master_MoveablePlatform.h
@@ -3,7 +3,6 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Components/InterpToMovementComponent.h"
 #include "GameFramework/Actor.h"
 #include "Master_MoveablePlatform.generated.h"
 
@@ -16,11 +15,47 @@ public:
 	// Sets default values for this actor's properties
 	AMaster_MoveablePlatform();
 
-  /* Whether or not the platform should wait for pressure pate input to start moving */
+  /* Visual representation of the platform */
+  UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+  class UPaperSpriteComponent* SpriteComp;
+
+  /* Location to move to */
+  UPROPERTY(EditAnywhere, meta = (MakeEditWidget = true), Category = "Platform Settings")
+  FVector TargetLocation;
+
+  /* The speed of this platform */
   UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Platform Settings")
+  float MovementSpeed;
+
+  /* Whether or not the platform should move to target location then stop or should it bounce back to the starting location */
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Platform Settings")
+  bool bOneShot;
+
+   /* Whether or not the platform should automatically move when game is started */
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Platform Settings", meta = (EditCondition = "!bStartWithPlate"))
+  bool bAutoStart;
+
+  /* Whether or not the platform should wait for pressure pate input to start moving */
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Platform Settings", meta = (EditCondition = "!bAutoStart"))
   bool bStartWithPlate;
 
   /* Function called to start platform movement */
-  UFUNCTION(BlueprintImplementableEvent, Category = "Platform Events")
+  UFUNCTION(BlueprintCallable, Category = "Platform Functions")
   void StartMovement();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+  
+  bool bCanMove;
+
+  FVector GlobalTargetLocation;
+
+  FVector GlobalStartLocation;
 };
